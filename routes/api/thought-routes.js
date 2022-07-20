bd633dfbcb42d2d15cef7a3901b104aa1ab0fa82
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-20 17:50:57 -0400

    thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought
+} = require('../../controllers/thought-controller');
+
+// /api/thought
+router
+  .route('/')
+  .get(getAllThought)
+  .post(createThought);
+
+// /api/thought/:id
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
