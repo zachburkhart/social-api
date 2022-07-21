aae3b6db21add399c31ce49bb8f32358bd3f542d
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-20 20:19:25 -0400

    finished models

--- models/User.js
+++ models/User.js
@@ -1,2 +1,43 @@
 const { Schema, model } = require('mongoose');
 const date = require('moment');
+
+const UserSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/,
+        },
+      thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+          }],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+        }]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false
+    }
+  );
+  
+  UserSchema.virtual('friendsCount').get(function() {
+    return this.friends.length;
+  });
+  
+  const User = model('User', UserSchema);
+  
+  module.exports = User;
